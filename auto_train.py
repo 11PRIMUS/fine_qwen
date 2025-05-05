d9b2512e754f15bc4b447d6fc8a6f0f9afe38934
11PRIMUS <alokpal2803@gmail.com>
2025-05-05 23:23:47 +0530

    added training

--- auto_train.py
+++ auto_train.py
@@ -1,19 +1,20 @@
 import torch
-from dataets import load_dataset
-from transformers import Autokenizer, AutoModelForCausalLM, TrainingArguments, Trainer, DataCollatorForLanguageModeling
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer, DataCollatorForLanguageModeling
 
 from peft import get_peft_model, LoraConfig, TaskType
 
 model_n="Qwen/Qwen1.5-0.5B"
-data=""
-dataset=load_dataset("json",data_f=data,split="train")
+data_path="emotion1.jsonl"
+dataset=load_dataset("json",data_files=data_path,split="train")
 
 def f_prompt(example):
     return{
-        "text":"f"### Emotion:{exmaple['emotion]}\n###n Input:{example["input"]}\n Response:{example['response]}""
+        "text":f"### Emotion:{example['emotion']}\n### Input:{example['input']}\n### Response:{example['response']}"
     }
 dataset=dataset.map(f_prompt)
-tokenizer=Autokenizer.from_pretrained(model_n,trust_remote_code=True)
+
+tokenizer=AutoTokenizer.from_pretrained(model_n,trust_remote_code=True)
 tokenizer.pad_token=tokenizer.eos_token
 
 def tokenize(example):
@@ -28,5 +29,26 @@ lora_config=LoraConfig(
     r=8,loara_alpha=32,target_modules=["q_proj","v_proj"],
     lora_dropout=0.1,bias="none",tasktype=TaskType.CAUSAL_LM
 )
-model=get_perft_model(model,lora_config)
+model=get_peft_model(model,lora_config)
 
+args=TrainingArguments( #training
+    output_dir="emotion2",
+    per_device_train_batch_size=4,
+    num_train_epochs=3,
+    learning_rate=2e-4,
+    fp16=True,
+    logging_dir="logs",
+    logging_steps=5,
+    save_strategy="epoch",
+)
+trainer=Trainer(
+    model=model,
+    args=args,
+    train_dataset=tokenized,
+    tokenizer=tokenizer,
+    data_collator=DataCollatorForLanguageModeling(tokenizer,mlm=False),
+
+)
+trainer.train()
+model.save_pretrained("emotion")
+tokenizer.save_pretrained("emotion")
